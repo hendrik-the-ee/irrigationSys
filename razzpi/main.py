73795112536df8c6948e5a682dbb85c277be395b
Heather Hendy <hh@bit.ly>
2019-04-13 23:03:09 -0600

    updates to return time to sleep

--- razzpi/main.py
+++ razzpi/main.py
@@ -1,9 +1,24 @@
 from flask import Flask, request
+import logging
 
 app = Flask(__name__)
 
-@app.route('/data', methods = ["POST", "GET"])
+logging.basicConfig(
+    filename="sensor_data.log",
+    format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.INFO
+)
+
+
+@app.route('/data', methods = ["POST"])
 def data():
-    return 'hello from the raspberry pi!'
+    # todo - add authentication for the esp32
+    logging.info(request.data)
+    # seconds for sleep;
+    # need to factor in waking from deep sleep and
+    # getting sensor info between requests
+    return '3590'
+
 
 app.run(host='0.0.0.0', port=8090)
