4cf2fc3c2c6a221d5a1b2a58891cd0dbdfebb247
Hendrik Gilmer <igotovt@gmail.com>
2019-04-14 22:37:47 -0600

    now sends multiple fields comma-separated data to the PI.

--- espSoil/espSoil.ino
+++ espSoil/espSoil.ino
@@ -1,15 +1,32 @@
+/*
+ * The Irrigation System
+ * 
+ * This program makes soil moisture measurements for a DIY irrigation system.  Specifically, ot does the following:
+ * 1. Connects via WiFi to a Raspberry Pi, 
+ * 2. Measures soil moisture and temperature from an Adafruit SeeSaw sensor, 
+ * 3. Transmits soil moisture and temperature to the Pi
+ * 4. Goes to sleep for a specific period of time.
+ * 
+ * Hendrik, 4/14/2019
+ * program memory (4/14): 900768 bytes.  506935 bytes compressed.
+ */
+
 // includes
+#include <EEPROM.h>
 #include "Adafruit_seesaw.h"
 #include <WiFi.h>
 #include "wifiStuff.h"
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 // definitions
-#define pinLed 5    // GPIO pin
-#define uS_TO_S_FACTOR 1000000  //Conversion factor for micro seconds to seconds
-//#define TIME_TO_SLEEP  10        //Time ESP32 will go to sleep (in seconds)
+#define pinLed 5                      // GPIO pin
+#define ESPID_EEPROM_ADDR 0           // address in EEPROM for ESP32 ID
+#define EEPROM_SIZE 1
+#define uS_TO_S_FACTOR 1000000        //Conversion factor for micro seconds to seconds
 
 // global variables
+
 uint16_t timeNow = millis();
 uint16_t timeLed = 200;
 Adafruit_seesaw ss;   // soil sensor
@@ -57,16 +74,17 @@ void wifiSetup() {
   } else {
     Serial.print("seesaw started! version: ");
     Serial.println(ss.getVersion(), HEX);
-  }
+  } // (!ss.begin)
+
   WiFi.begin(wifiSsid, wifiPass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");  
-} // wifiSetup
+} // void wifiSetup
 
-void wifiSendData(uint16_t wifiDataToSend) {
+void wifiSendData(String wifiDataToSend) {
   if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
     HTTPClient http;
     http.begin(httpAddr);
@@ -76,8 +94,6 @@ void wifiSendData(uint16_t wifiDataToSend) {
     if(httpResponseCode>0){
       String timeToSleepString = http.getString();
       timeToSleep = timeToSleepString.toInt();      
-//      Serial.println("httpResponseCode=" + String(httpResponseCode) + ", timeToSleep=" + String(timeToSleep));
-      esp_sleep_enable_timer_wakeup(timeToSleep * uS_TO_S_FACTOR);
     }else{
       Serial.println("HTTP POST Error");
     }  // httpResponseCode
@@ -85,48 +101,75 @@ void wifiSendData(uint16_t wifiDataToSend) {
   } // if(WiFi.status()
   else{
     Serial.println("Error in WiFi connection");
-  }
-}  // wifiSendData
+  } // if(WiFi.status())
+
+  esp_sleep_enable_timer_wakeup(timeToSleep * uS_TO_S_FACTOR);
+}  // void wifiSendData
 
 void setup() {
-  pinMode(pinLed, OUTPUT);
-  
+//  int espID = EEPROM.read(ESPID_EEPROM_ADDR);
+
   Serial.begin(115200);
+  pinMode(pinLed, OUTPUT);
+
+/*  if (espID == NULL) {
+    Serial.println("Enter ESPID (value from 0 to 255): ");
+    espID = Serial.read();
+    EEPROM.write(ESPID_EEPROM_ADDR, espID);
+  }else{
+    Serial.println("ESPID=" + String(espID));
+  }     // IF (espID)
+*/
+
   bootCount++;
 //  Serial.println("BootCount=" + String(bootCount));
 //  print_wakeup_reason();  //Print the wakeup reason for ESP32
-
   wifiSetup();
-
 }  // setup
 
 void loop() {
   uint16_t timeDiff;
   uint16_t timeMillis = uint16_t(millis());
-  const uint16_t timeSoilSample = 2000; //milliseconds
+  const uint16_t timeSampleData = 2000; //milliseconds
   const uint16_t u16Max = 65535;
-  const uint16_t timeWifi = 5000; // milliseconds
   uint16_t ssData;
+  int testRead;
+  const int espID = 1;
+  
+//  char PostData[] = "{\"name\": \"Fred\", \"age\": 31}"; // your JSON payload
+  String PostData =  "esp-000";
 
   // set value for timeDiff
-  if ((timeMillis<timeSoilSample) && ((u16Max-timeNow)<timeSoilSample)) { // check for timeNow just about to wrap around, and millis() already did
+  if ((timeMillis<timeSampleData) && ((u16Max-timeNow)<timeSampleData)) { // check for timeNow wrap around
     timeDiff = timeMillis + uint16_t(u16Max-timeNow);
   }
-  else {
+  else {                                  // no wraparound
     timeDiff = timeMillis - timeNow;
-  }
-  if (timeDiff > timeSoilSample)
+  } // if (timeMillis)
+  
+  if (timeDiff > timeSampleData)
   {
     loopCount++;
-    ssData = ssReadAndPrint();             // do soil sensor stuff
-    wifiSendData(ssData);
+//    ssData = ssReadAndPrint();             // do soil sensor stuff    
+
+    // String stuff
+    PostData.concat(espID);
+    PostData.concat("\r\n");
+    PostData.concat("s_0001a,soil_sensor,");
+    PostData.concat(ss.getTemp());
+    PostData.concat(", ");
+    PostData.concat(ss.touchRead(0));
+    PostData.concat("\r\n");
+    wifiSendData(PostData);
+    Serial.println(PostData);
+
     digitalWrite(pinLed, HIGH);   // turn the LED on (HIGH is the voltage level)
     timeNow = timeMillis;         // reset timer
   }
   else if ((digitalRead(pinLed)==1) && (timeDiff > timeLed))  // turn off LED
   {
     digitalWrite(pinLed, LOW);   // turn the LED on (HIGH is the voltage level)
-  } // timeDiff
+  } // if (timeDiff)
 
   if (loopCount >= 1) {
       esp_deep_sleep_start();
