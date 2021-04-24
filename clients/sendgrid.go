5b9ee00f28167f71081d54000adf2066b3ba2c41
Heather Hendy <hlhendy@gmail.com>
2021-04-24 11:07:23 -0600

    removes unused code

--- clients/sendgrid.go
+++ clients/sendgrid.go
@@ -3,13 +3,10 @@
 package clients
 
 import (
-	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
-	"golang.org/x/net/html"
 )
 
 const (
@@ -58,60 +55,4 @@ func (e *Email) Send(soilTemp float32, warningType string) error {
 	}
 
 	return nil
-}
-
-type Message struct {
-	RecipientEmail string `json:"to"`
-	RecipientName  string `json:"to_name"`
-	SenderEmail    string `json:"from"`
-	SenderName     string `json:"from_name"`
-	Subject        string `json:"subject"`
-	HTMLBody       string `json:"body"`
-
-	// PlainTextBody stores the body of the email with HTML stripped out
-	PlainTextBody string `json:"plain_text_body,omitempty"`
-}
-
-// Validate does some basic checks on the data and populates the PlainTextBody field
-func (m *Message) Validate() error {
-
-	if len(m.RecipientEmail) > MaxEmailAddressLength {
-		return errors.New("INVALID_ARG_TO_EXCEEDS_MAX_LENGTH")
-	}
-
-	if len(m.SenderEmail) > MaxEmailAddressLength {
-		return errors.New("INVALID_ARG_FROM_EXCEEDS_MAX_LENGTH")
-	}
-
-	if len(m.Subject) > MaxSubjectLineLength {
-		return errors.New("INVALID_ARG_SUBJECT_EXCEEDS_MAX_LENGTH")
-	}
-
-	m.PlainTextBody = getPlainText(m.HTMLBody)
-
-	return nil
-
-}
-
-func getPlainText(htmlBody string) string {
-	tokenizer := html.NewTokenizer(strings.NewReader(htmlBody))
-	prevToken := tokenizer.Token()
-
-	var plainText string
-
-tokenizerLoop:
-	for {
-		t := tokenizer.Next()
-		switch {
-		case t == html.ErrorToken:
-			break tokenizerLoop // end of html
-		case t == html.TextToken:
-			if prevToken.Data == "script" {
-				continue
-			}
-			text := html.UnescapeString(string(tokenizer.Text()))
-			plainText += strings.TrimSpace(text)
-		}
-	}
-	return plainText
 }
